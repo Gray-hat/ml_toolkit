0af588a6bb10ee5d98e6faf3244519c137271493
Ian Kosen <ikosenn@gmail.com>
2015-03-25 02:20:41 +0300

    Finished backprop and add a testing mechanism

--- Algorithms/neural_networks/activation_functions.py
+++ Algorithms/neural_networks/activation_functions.py
@@ -1,10 +1,15 @@
 #a file implemeting some of the known activation functions
 #for the neural networks
-
+import math
+import numpy as np
 def sigmoid_function(v):
 		'''
 		An activation function
 		'''
+		if  isinstance(v,np.matrixlib.defmatrix.matrix):
+			for x in np.nditer(v, op_flags = ['readwrite']):
+				x[...] = (1 / (1 + math.exp(-x)))
+			return v
 		return (1 / (1 + math.exp(-v)))
 
 def threshold_function(threshold, v):
@@ -12,13 +17,24 @@ def threshold_function(threshold, v):
 	An activation function
 	'''	
 	#error checking not done
+	if  isinstance(v,np.matrixlib.defmatrix.matrix):
+			for x in np.nditer(v, op_flags = ['readwrite']):
+				if x >= threshold:
+					x[...] = 1
+				else:
+					x[...] = 0
+			return v
 	if v >= threshold:
 		return 1
 	else:
 		return 0
 
-def line_equation(x):
+def line_equation(v):
 	'''The equation of a line y = mx + c '''
 	c,m = (0,1)
 
-	return (m * x) + c 
+	if  isinstance(v,np.matrixlib.defmatrix.matrix):
+			for x in np.nditer(v, op_flags = ['readwrite']):
+				x[...] = (m * x) + c 
+			return v
+	return (m * v) + c 
--- Algorithms/neural_networks/neuralnet.py
+++ Algorithms/neural_networks/neuralnet.py
@@ -6,6 +6,7 @@ import os
 import math
 import random
 import numpy as np
+from pylab import plot, ylim, show
 
 from activation_functions import threshold_function
 from activation_functions import sigmoid_function
@@ -26,14 +27,18 @@ class NeuralNetwork():
 		self.data_set = data_set
 		self.normalized = []
 		self.weights = []
+		self.average_errors = []
 		self.algorithm = algorithm #0 = Perptron 1 = adaline 2 = backprop
 		self.learning_rate = learning_rate
 		self.threshold = threshold
+		self.test_set = []
 		self.bias = bias
 		self.add_bias()
 		self.set_initial_weights()
 		self.normalize()
+		self.split_dataset()
 
+		assert (algorithm == 2 and hidden_layer) or (algorithm ==1) or (algorithm ==0), "Hidden layer must be defined"
 
 	def set_initial_weights(self):
 		'''
@@ -69,7 +74,7 @@ class NeuralNetwork():
 			if isinstance(self.weights[i][0], list):
 				#weights is an array
 				for j in range(len(self.weights[i])):
-					print("Layer {0}  node {1} are: {2}".format(i, j, "".join(str(self.weights[i]))))
+					print("Layer {0}  node {1} are: {2}".format(i, j, "".join(str(self.weights[i][j]))))
 			else: 
 				print("Layer {0} are: {1}".format(i, "".join(str(self.weights[i]))))		
 
@@ -79,10 +84,16 @@ class NeuralNetwork():
 			Add bias to the data
 		'''
 		for data in self.data_set:
-			data.insert(0, str(self.bias)) #making it a string to enable comparisons on the normalization phase
+			data.insert(0, self.bias) #making it a string to enable comparisons on the normalization phase
 
 
+	def split_dataset(self):
+		'''splits the dataset to test and training data'''
 
+		size = int(0.2 * len(self.normalized)) or 1
+		self.test_set.append(self.normalized[-size])
+
+		self.normalized = self.normalized[:len(self.normalized) - size]
 
 
 	def normalize(self):
@@ -90,6 +101,8 @@ class NeuralNetwork():
 		temp = []
 		#flatten array
 		list(map(lambda x: temp.extend(x),self.data_set))
+		temp = [int(i) for i in temp]
+		print(temp)
 		largest = float(max(temp))
 		self.normalized = list(self.data_set)
 
@@ -121,16 +134,17 @@ class NeuralNetwork():
 					temp = np.matrix(inputs)
 					weights = np.matrix(self.weights[0]).T
 					product = np.dot(temp, weights)
-					summation = sum(product)
+
 
 					if self.algorithm ==0:
-						activated = threshold_function(self.threshold, summation.item())
+						activated = threshold_function(self.threshold, product.item())
 					else:
-						activated = line_equation(summation.item())	
+						activated = line_equation(product.item())	
 				
 					print("The current weights and model outputs are {0} and {1} respectively".format(weights, activated))
 					
 					error = d_output - activated
+					self.average_errors.append(error)
 					errors.append(error * error)
 					weight_change = temp.T * (self.learning_rate * error) 
 					print("The models error is {0} and the change in weights is {1}".format(error, weight_change))
@@ -145,8 +159,6 @@ class NeuralNetwork():
 				if round(average_error,1) <= self.tolerance:
 					print("The desired error tolerance has been achieved.")
 					print("Exiting... ")
-					print("The weights for the model are:")
-					self.output_weights()
 					flag = False
 					break
 						
@@ -160,21 +172,146 @@ class NeuralNetwork():
 		self.output_weights()	
 
 
-	def perceptron(self):
-		'''
-			Method to implement the perceptron 
-		'''
-		
-	def adaline(self):
-		'''
-		'''
-		pass
-
 	def backprop(self):
 		'''
+			Backprop learning and training
 		'''
-		pass				
 
+		x = 0
+		flag = True
+		#infinite loop
+		while flag:
+			#repeat the epoch until there is a convergence
+			print('******************** Start of Epoch {0} ********************'.format(x))
+			errors = []
+			for data in self.normalized:
+				#loop thru each data input
+				inputs = data[:-1]
+				d_output = data[-1:][0]
+				print("The inputs to the network are {0} and the desired output is {1}".format(inputs, d_output))
+
+				input_0 = np.matrix(inputs)
+				output_0 = line_equation(input_0)
+				print("The outputs of the input layer are {0}".format(output_0))
+				weights = np.matrix(self.weights[0]).T
+				input_1 = np.dot(weights, input_0.T)
+				print("The inputs of the hidden layer are {0}".format(input_1))
+				output_1 = sigmoid_function(input_1)
+				print("The outputs of the hidden layer are {0}.".format(output_1))
+				input_2 = np.dot(self.weights[1], output_1)
+				print("The input to the output layer is {0}".format(input_2))
+				output_2 = sigmoid_function(input_2.item())
+				print("The output to the output layer is {0}".format(output_2))
+
+				error = d_output - output_2
+				self.average_errors.append(error)
+				errors.append(error * error)
+
+				#calculate weight for the second weights
+
+				d = (d_output - output_2) * output_2 * (1 - output_2)
+				weight_change_w = self.learning_rate * d * output_1
+
+				#calculate weights for the first weights
+
+				w = np.matrix(self.weights[1]).T * d
+				a, b, g, h = (w[0].item(), w[1].item(), output_1[0].item(), output_1[1].item())
+
+				weight_change_v = [[a * g * (1 - g)],[b * h * (1 - h)]]
+				weight_change_v = np.matrix(weight_change_v).T
+				#update weights
+				print("Updating weights.......")
+
+				self.weights[0] = ((output_0.T * weight_change_v) * self.learning_rate) + self.weights[0]
+
+
+				self.weights[1] = np.matrix(self.weights[1]).T + weight_change_w
+				self.weights[1] = self.weights[1].T
+
+				print("The updated weights are:")
+				self.output_weights()
+			print('******************** End of Epoch {0}   ********************'.format(x))
+
+			average_error = np.mean(errors)
+			print("The average error is, {0}".format(average_error))
+			if round(average_error,1) <= self.tolerance:
+				print("The desired error tolerance has been achieved.")
+				print("Exiting... ")
+				flag = False
+				break
+					
+			
+			x += 1	
+						
+	def test(self):
+		'''Test the model to check if it conforms to
+		the standards'''
+		#true class
+		class_p = 1
+		class_n = 0
+
+		tp = 0
+		fn = 0
+		fp = 0
+		tn = 0
+
+		print("******************TESTING**************")
+	
+
+		for data in self.test_set:
+			#loop thru each data input
+			inputs = data[:-1]
+			d_output = data[-1:][0]
+			print("The inputs to the network are {0} and the desired output is {1}".format(inputs, d_output))
+
+			if self.algorithm == 0 or self.algorithm == 1:
+
+				temp = np.matrix(inputs)
+				weights = np.matrix(self.weights[0]).T
+				product = np.dot(temp, weights)
+
+				if self.algorithm ==0:
+					m_output = threshold_function(self.threshold, product.item())
+				else:
+					m_output = line_equation(product.item())
+				print("The models output is {0}".format(m_output))		
+
+			else:
+				input_0 = np.matrix(inputs)
+				output_0 = line_equation(input_0)
+				print("The outputs of the input layer are {0}".format(output_0))
+				weights = np.matrix(self.weights[0]).T
+				input_1 = np.dot(weights, input_0.T)
+				print("The inputs of the hidden layer are {0}".format(input_1))
+				output_1 = sigmoid_function(input_1)
+				print("The outputs of the hidden layer are {0}.".format(output_1))
+				input_2 = np.dot(self.weights[1], output_1)
+				print("The input to the output layer is {0}".format(input_2))
+				m_output = sigmoid_function(input_2.item())
+				print("The output to the output layer is {0}".format(m_output))	
+
+			if m_output == d_output == class_p:
+				tp += 1
+			elif m_output == d_output == class_n:
+				tn += 1
+			elif m_output == class_p and d_output == class_n:
+				fp += 1
+			elif m_output == class_n and d_output == class_p:
+				fn += 1	
+			else:
+				print("An error has occured")			
+
+
+
+		print("**********COMPLETE***************")
+		print("Analysis....")
+
+		recall = tp / (tp + fn)
+		precision = tp / (tp + fp)
+		f_score = (2 * (precision * recall)) / (precision + recall)
+		print("The recall is: {0}".format(recall))
+		print("The precision is: {0}".format(precision))
+		print("The F-score is: {0}".format(f_score))
 
 
 
@@ -189,12 +326,27 @@ if __name__ == '__main__':
 
 	#remove the data lables
 	labels = training_set.pop(0)
-
+	
 	neuron = NeuralNetwork(training_set, 0, 0.1, 0.1)
+	print(neuron.weights)
 	neuron.output_weights()
 	print(neuron.normalized)
 	neuron.iterate()
+	neuron.test()
 
-	   
+#backprop
+
+	# neuron = NeuralNetwork(training_set, 2, 0.1, 0.1, bias = 0, hidden_layer = 2)
+	# print(neuron.weights)
+	# neuron.output_weights()
+	# print(neuron.normalized)
+	# neuron.iterate()
+	# neuron.test()
+
+#plot graph
+ylim([-1, 1])
+plot(neuron.average_errors)
+show()
+
 	
 		
